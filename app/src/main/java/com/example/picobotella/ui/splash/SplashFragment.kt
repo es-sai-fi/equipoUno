6103342ba51e4e0d1015648b2c902a6381e019af
es-sai-fi <athousandheavenlycats@protonmail.com>
2026-05-31 18:33:49 -0500

    feat: rate app and share app intents

--- app/src/main/java/com/example/picobotella/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/picobotella/ui/splash/SplashFragment.kt
@@ -1,9 +1,11 @@
 package com.example.picobotella.ui.splash
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.picobotella.R
@@ -11,7 +13,7 @@ import com.example.picobotella.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
 
-  lateinit var binding: FragmentSplashBinding
+  private lateinit var binding: FragmentSplashBinding
 
   override fun onCreateView(
       inflater: LayoutInflater,
@@ -29,5 +31,59 @@ class SplashFragment : Fragment() {
     super.onViewCreated(view, savedInstanceState)
 
     binding.bottleIcon.animate().rotation(360f).setDuration(2000).start()
+
+    binding.rateButton.setOnClickListener { rateApp() }
+
+    binding.shareButton.setOnClickListener { shareApp() }
+
+    /*
+    view.postDelayed({
+
+        if (isAdded) {
+
+            findNavController().navigate(
+                R.id.action_splashFragment_to_homeFragment
+            )
+
+        }
+
+    }, 5000)
+    */
+  }
+
+  private fun rateApp() {
+
+    val intent =
+        Intent(
+            Intent.ACTION_VIEW,
+            "https://play.google.com/store/apps/details?id=com.nequi.MobileApp&hl=es_419&gl=es"
+                .toUri(),
+        )
+
+    startActivity(intent)
+  }
+
+  private fun shareApp() {
+
+    val shareText =
+        """
+        App Pico Botella
+
+        Solo los valientes lo juegan !!
+
+        https://play.google.com/store/apps/details?id=com.nequi.MobileApp&hl=es_419&gl=es
+        """
+            .trimIndent()
+
+    val intent =
+        Intent().apply {
+          action = Intent.ACTION_SEND
+
+          putExtra(Intent.EXTRA_TEXT, shareText)
+
+          type = "text/plain"
+        }
+
+    startActivity(Intent.createChooser(intent, "Compartir aplicación"))
   }
 }
